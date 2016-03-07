a899c9e00e2ff231eeba5a2b0ffc87ab55d44bcb
stephanieyang <stephanie.yang.121@gmail.com>
2016-03-07 17:05:30 -0500

    adding picture bank file

---
+++ js/imageBank.js
@@ -0,0 +1,22 @@
+var imageList = [
+{
+	imageSrc : 'fossil.gif',
+	category : 'geology'
+},
+{
+	imageSrc : 'plant',
+	category : 'botany'
+},
+{
+	imageSrc : 'incubator',
+	category : 'biology'
+},
+{
+	imageSrc : 'dropper',
+	category : 'chemistry'
+},
+{
+	imageSrc : 'star',
+	category : 'astronomy'
+},
+];
