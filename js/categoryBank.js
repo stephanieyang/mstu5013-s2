84f2aeb41b3750a8907d40bbd6db70fb14b0a9bd
stephanieyang <stephanie.yang.121@gmail.com>
2016-03-21 21:25:46 -0400

    adding category bank

---
+++ js/categoryBank.js
@@ -0,0 +1,36 @@
+// Sorted by alphabetical order
+var categoryBank = {
+	'art': {
+		'art': [];
+		'biology': [];
+		'chemistry': [];
+		'computerScience': [];
+		'math': [];
+		'physics': [];
+	},
+	'biology': {
+		'biology': [];
+		'chemistry': [];
+		'computerScience': [];
+		'math': [];
+		'physics': [];
+	},
+	'chemistry': {
+		'chemistry': [];
+		'computerScience': [];
+		'math': [];
+		'physics': [];
+	},
+	'computerScience': {
+		'computerScience': [];
+		'math': [];
+		'physics': [];
+	},
+	'math': {
+		'math': [];
+		'physics': [];
+	},
+	'physics': {
+		'physics': [];
+	},
+};
